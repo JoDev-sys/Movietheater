0b52b5b61455fc609a3ee05e4a9707a6257218a1
Joselito Penas <jopenas23@gmail.com>
2026-02-01 22:09:34 +0800

    Added Home page, array of movies, and used .map

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import MovieCard from "../components/MovieCard";
+
+
+function Home() {
+    const movies = [
+        { id: 1, title: "Inception", release_date: "2010-07-16" },
+        { id: 2, title: "The Matrix", release_date: "1999-03-31" },
+        { id: 3, title: "Interstellar", release_date: "2014-11-07" }
+    ];
+
+  return (
+    <div className="home">
+          <div className="movies-grid">
+              {movies.map((movie) => (
+                <MovieCard movie={movie} key={movie.id} />
+            ))}
+        </div>
+    </div>
+  );
+}
+
+export default Home;
