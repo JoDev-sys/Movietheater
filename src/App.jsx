11d474e3a66a762a6a376490ad521f323ceea83a
Joselito Penas <jopenas23@gmail.com>
2026-02-14 23:04:18 +0800

    Added NavBar to navigate from home page to favorites

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import './App.css'
 import Favorites from './pages/Favorites';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
+      <div>
+        <NavBar />
+      </div>
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
